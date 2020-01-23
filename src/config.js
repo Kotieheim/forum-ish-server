9d5dcb4080a1861ba825a6dd166868bae2cbad84
Dakota Heim <kotieheim@gmail.com>
2020-01-23 09:38:43 -0700

    fix database env

--- src/config.js
+++ src/config.js
@@ -2,10 +2,10 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   API_TOKEN: process.env.API_TOKEN || "dummy-api-token",
-  DB_URL:
+  DATABASE_URL:
     process.env.DATABASE_URL ||
     "postgresql://dunder_mifflin:password@localhost/forum-ish",
-  TEST_DB_URL:
+  TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
     "postgresql://dunder_mifflin:password@localhost/forum-ish-test",
   CLIENT_ORIGIN: "http://localhost:3000",
